c90d2b445980ea4cd3ae5cee22796d53e5ec49f6
joao147 <j218927@dac.unicamp.br>
2021-01-21 16:21:18 -0300

    added generete items and emails functions and update README

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # ChallengeOfStone
 I aplly to a work with elixir language, and the first filter is a programming challenge.
 
+# Pseudorandom generetions
+I did two functions to generete somo items and emails, you can change the maximum number of generete, 
+just change this constants numberOfMaxGenereteItems and/or numberOfMaxGenereteEmails
+I put as default 100 to both constants.
+
 # How execute
 1° You need a package manager to node, like npm or yarn in your pc.
 2° So you'll use "npm i" or "yarn" to download dependencies.
 3° To execute, with npm use "npx ts-node ./challenge.ts", and with yarn "yarn ts-node ./challenge.ts".
+4° See the result.
 
 # Github of challenge 
-link of challenge description: https://gist.github.com/programa-elixir/1bd50a6d97909f2daa5809c7bb5b9a8a
+link of challenge description: https://gist.github.com/programa-elixir/1bd50a6d97909f2daa5809c7bb5b9a8a.
--- challenge.ts
+++ challenge.ts
@@ -1,7 +1,7 @@
 //type of items array
 type itemsType = Array<{
   product: string,
-  unitPrice: number,
+  unitCost: number,
   quantity: number
 }>;
 
@@ -18,7 +18,7 @@ const calcAndSplitPrice = ( items: itemsType, emails: emailsType) => {
 
   //foreach to get total cost
   items.forEach(item => {
-    totalCost = totalCost + (item.quantity * item.unitPrice);
+    totalCost = totalCost + (item.quantity * item.unitCost);
   });
 
   //here I get the floor of division, javascript don t have int, 
@@ -48,11 +48,58 @@ const calcAndSplitPrice = ( items: itemsType, emails: emailsType) => {
   return resultMap;
 }
 
+//generete items
+const genereteItems = (maxGenerete: number) => {
+
+  let numberOfItems = Math.floor(Math.random() * maxGenerete);
+
+  let generetedItems: itemsType = [];
+
+  for(let i = 0; i < numberOfItems; i++){
+    if(i%2){
+      generetedItems.push({product: `something${i}`, unitCost: Math.ceil(Math.random() * i), quantity: 1});
+    }else {
+      generetedItems.push({product: `otherthing${i}`, unitCost:  i, quantity: Math.ceil(Math.random() * i)});
+    }
+  }
+
+  return generetedItems;
+}
+
+//generete emails
+const genereteEmails = (maxGenerete: number) => {
+
+  let numberOfEmails = Math.floor(Math.random() * maxGenerete);
+
+  let generetedEmails: emailsType = [];
+
+  for(let i = 0; i < numberOfEmails - 2; i++){
+    if(i%2){
+      generetedEmails.push(`joao${i}@dac.unicamp.br`);
+    }else {
+      generetedEmails.push(`joao${i}@gmail.com`);
+    }
+  }
+
+  if(numberOfEmails >= 3){
+    //my emails :))
+    generetedEmails.push('turenkk4@gmail.com');
+
+    generetedEmails.push('j218927@dac.unicamp.br');
+  }
+
+  return generetedEmails;
+}
+
 //function main
 const main = () => {
 
-  const items: itemsType = [];
-  const emails: emailsType = [];
+  //you can chande this number and reduce ou increase the max number of genetere items or emails
+  const numberOfMaxGenereteItems = 100;
+  const numberOfMaxGenereteEmails = 100;
+
+  const items: itemsType = genereteItems(numberOfMaxGenereteItems);//generete some "random" items
+  const emails: emailsType = genereteEmails(numberOfMaxGenereteEmails);//generete some "random" emails
 
   //tests if the lists are empties
   if(items.length === 0 && emails.length === 0){
@@ -62,13 +109,12 @@ const main = () => {
   }else if(emails.length === 0){
     console.log('No email to divide the costs');
   }else{
-    //a const to store the fuction return
+    //a constant to store the fuction return
     const result: Map<string, number> = calcAndSplitPrice(items, emails);
 
     //print in console the result as a Map
     console.log(result);
   }
-  
 }
 
 //call the main function
